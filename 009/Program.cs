847fabcb1bed7df6988d12e45f80f599715f9f74
Ivan_M <molivan@yandex.ru>
2022-11-22 20:50:22 +0400

    test2

--- 009/Program.cs
+++ 009/Program.cs
@@ -1,3 +1,14 @@
-﻿int a = Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine();
+﻿//int a = Convert.ToInt32 (Console.ReadLine());
+//Console.WriteLine();
+
+int b = int.Parse(Console.ReadLine());
+int num = b + 3;
+System.Console.WriteLine(num);
+
+
+
+
+
+
+
 
