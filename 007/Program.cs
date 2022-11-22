2a5c06b9e2102cade5a4d326103492e09c996b8e
Ivan_M <molivan@yandex.ru>
2022-11-22 23:49:27 +0400

    задача 10

--- 007/Program.cs
+++ 007/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿/*
+Задача 10: 
+Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+
+
+Console.WriteLine("Введите трехзначное число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int result = (numberA % 100) / 10;
+System.Console.WriteLine("Вторая цифра числа " + numberA + " = " + result);
