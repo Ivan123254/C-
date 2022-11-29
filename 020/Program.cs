c6803df1302f93fff9c02035eecaf5b4d6a61f19
Ivan_M <molivan@yandex.ru>
2022-11-29 14:08:51 +0400

    1223

---
+++ 020/Program.cs
@@ -0,0 +1,10 @@
+﻿string NumbersFor(int a, int b)
+{
+    string result = string.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+System.Console.WriteLine(NumbersFor(2,10));
